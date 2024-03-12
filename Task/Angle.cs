61eec81552ea67e07409bf1a4e9ecc1444755614
Денис <mden1996.07@gmail.com>
2024-03-12 17:09:13 +0300

    Добавьте файлы проекта.

---
+++ Task/Angle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Angle
+    {
+        int gradus { get; set; }
+        int min { get; set; }
+        int sec { get; set; }
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if(value < 0) {
+                    Console.WriteLine("Градусы меньше 0");
+                }
+                else {
+                    gradus = value;
+                }
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if(value < 0) {
+                    Console.WriteLine("Минуты меньше 0");
+                }
+                else {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if(value < 0) {
+                    Console.WriteLine("Секунды меньше 0");
+                }
+                else {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+        public Angle(int gradus, int minutes, int seconds)
+        {
+            this.Gradus = gradus;
+            this.Min = minutes;
+            this.Sec = seconds;
+        }
+
+        public double ToRadians()
+        {
+            double res = (Gradus + Min / 60.0 + Sec / 3600.0) * (Math.PI / 180);
+            return res;
+        }
+
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            int minutes = Convert.ToInt32(Console.ReadLine());
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            Angle angle = new Angle(gradus, minutes, seconds);
+            //Angle angle2 = new Angle(5, 30, 30);
+            Console.WriteLine("Radians = {0}", angle.ToRadians());
+            Console.WriteLine("deg = {0} min = {1} sec = {2}", angle.Gradus, angle.Min, angle.Sec);
+            Console.ReadKey();
+        }
+    }
+}
